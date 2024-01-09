660fda62f27d887bae50b4c9cc68668d8d0261d5
puruz14 <149493113+puruz14@users.noreply.github.com>
2024-01-09 23:33:39 +0530

    Update Median_Sorted_Arrays.cpp

--- 9 Jan/Median_Sorted_Arrays.cpp
+++ 9 Jan/Median_Sorted_Arrays.cpp
@@ -16,7 +16,7 @@ public:
             if(mid1-1>=0) l1=nums1[mid1-1];
             if(mid2-1>=0) l2=nums2[mid2-1];
             if(l1<=r2&&l2<=r1){
-                if(n1%2) return max(l1,l2);
+                if(n%2) return max(l1,l2);
                 return (max(l1,l2)+min(r1,r2))/2.0;
             }
             else if(l1>r2) high=mid1-1;
