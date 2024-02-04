52fc0b74351a7fd9167f630445ee410580eff195
puruz14 <149493113+puruz14@users.noreply.github.com>
2024-02-04 17:53:53 +0530

    Create DIFFSTR.cpp

---
+++ 04 Feb/DIFFSTR.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string a[n];
+        for(int i=0;i<n;i++) cin>>a[i];
+        for(int i=0;i<n;i++) cout<<'1'-a[i][i];
+        cout<<endl;
+    }
+    return 0;
+}
